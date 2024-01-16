0fc2095ee39188bb609be4412c7103d579ac5227
andr3w-coderman <admyrden@gmail.com>
2024-01-16 10:50:24 -0500

    Window Name and Icon
    -Update window name to "Gordon's Cookoff"
    -Update window icon to Gordon Ramsay pixel portrait

--- variables.py
+++ variables.py
@@ -1,15 +1,15 @@
 import pygame
 
 # Images #
-
+iconImage = pygame.image.load('images/icon.png')
 
 # # # # # # # # # # # # # # # # # # # # # # # # #
 # Essentials #
 pygame.init()
 pygame.font.init()
 clock = pygame.time.Clock()  #Force frame rate to be slower
-pygame.display.set_caption('3rd Floor') # change window name
-#pygame.display.set_icon(iconImage) # change window icon
+pygame.display.set_caption("Gordon's Cookoff") # change window name
+pygame.display.set_icon(iconImage) # change window icon
 gameState = "menu"
 
 ev = pygame.event.poll() # EVENT POLL
